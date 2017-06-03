d3e5fcf7c7afabc59e370b60be32a25086bdfe49
jbhouse <jbmoorhouse1@gmail.com>
2017-06-02 19:30:22 -0500

    updated user model to accept tokens and updated controller to handle logging out

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  acts_as_token_authenticatable
   has_many :posts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
