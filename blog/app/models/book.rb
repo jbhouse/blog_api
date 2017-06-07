c59147dbea38b5072ac7f737ec373162f5bc5fef
jbhouse <jbmoorhouse1@gmail.com>
2017-06-07 18:48:24 -0500

    renamed categories to books

--- blog/app/models/book.rb
+++ blog/app/models/book.rb
@@ -1,4 +1,4 @@
-class Category < ApplicationRecord
+class Book < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
   belongs_to :writer, class_name: "User"
--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,20 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170607225030) do
+ActiveRecord::Schema.define(version: 20170607233826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "categories", force: :cascade do |t|
+  create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "description"
     t.bigint "writer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
-    t.index ["slug"], name: "index_categories_on_slug", unique: true
-    t.index ["writer_id"], name: "index_categories_on_writer_id"
+    t.index ["slug"], name: "index_books_on_slug", unique: true
+    t.index ["writer_id"], name: "index_books_on_writer_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -54,6 +54,6 @@ ActiveRecord::Schema.define(version: 20170607225030) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "categories", "users", column: "writer_id"
+  add_foreign_key "books", "users", column: "writer_id"
   add_foreign_key "posts", "users"
 end
